6fcdf4a05fb49788bb8696aefcb1528a53817861
Joël Ipenburg <joel.ipenburg@student.hu.nl>
2020-02-26 17:26:24 +0100

    Added more objects to medicine list. (change the get and set to java style).

--- MedicijnLijst.cs
+++ MedicijnLijst.cs
@@ -6,13 +6,31 @@ namespace ZorgAppOop
 {
     class MedicijnLijst
     {
+        //properties en fields
         public List<Medicijn> medicijnLijst { get; private set; }
+        //constructor
         public MedicijnLijst()
         {
+            //maakt de lijst wakker(initiateert) in property medicijnLijst:
             medicijnLijst = new List<Medicijn>();
-            medicijnLijst.Add(new Medicijn(1, "Paracetemol", "", "", ""));
-            medicijnLijst.Add(new Medicijn(2, "Asperine", "Omschrijving", "Soort", "Dosering"));
+            //vult property medicijn lijst in:
+            medicijnLijst.Add(new Medicijn(1, "Paracetemol",
+                "Paracetamol is een veelgebruikt pijnstillend en koortsverlagend middel.", 
+                "n.v.t.", 
+                "Maximaal 2 per dag"));
+            medicijnLijst.Add(new Medicijn(2, "Asperine", 
+                "Asperine is een medicijn dat pijnstillend, koortsverlagend en ontstekingsremmend werkt.",
+                "Salicylaten",
+                "Volwassenen 500-1000 mg per keer, maximaal 4000 mg per 24 uur"));
+            medicijnLijst.Add(new Medicijn(3, "Ibuprofen",
+                "Het werkt ontstekingsremmend, pijnstillend en koortsverlagend; de werking is vergelijkbaar met die van acetylsalicylzuur.",
+                "NSAID",
+                "De dosering ibuprofen voor volwassenen is maximaal 1200 mg per dag (24 uur)."));
+            medicijnLijst.Add(new Medicijn(4, "Naproxen", 
+                "Naproxen is een medicijn dat oorspronkelijk is ontwikkeld voor de behandeling van reumatische gewrichtsklachten.",
+                "Arylpropionzuurderivaten", 
+                "In Nederland is Naproxen zonder doktersrecept verkrijgbaar als pijnstiller in een dosering tot 275 mg"));
         }
-    }
+    }   
 
 }
