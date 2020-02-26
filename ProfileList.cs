b031386fa407ff55021bc1e5d141f5d2bdadb8ab
Joël Ipenburg <joel.ipenburg@student.hu.nl>
2020-02-26 17:29:11 +0100

    Added id array to profile list as arguments.

--- ProfileList.cs
+++ ProfileList.cs
@@ -15,11 +15,13 @@ namespace ZorgAppOop
                             I=> VoorNaam type of String #foreach number 2
                                       I=> "Frank" type of value
         */
+
+        //new Profile(int profileId, array medicijnId, string voornaam, string achternaam, int leeftijd, double gewicht, double lengte)
         public ProfileList() 
         {
             profileList = new List<Profile>();
-            profileList.Add(new Profile(1, "Frank", "Krijnen", 26, 65.15, 1.80));
-            profileList.Add(new Profile(2, "Joel", "Ipenburg", 27, 83.64, 1.88));
+            profileList.Add(new Profile(1, new int[] {3 , 4}, "Frank", "Krijnen", 26, 65.15, 1.80));
+            profileList.Add(new Profile(2, new int[] {1 , 2},"Joel", "Ipenburg", 27, 83.64, 1.88));
         }
        
 
